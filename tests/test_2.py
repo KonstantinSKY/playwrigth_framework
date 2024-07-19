5198800605fafd7a15ba738578266d49bb0e42f7
SKY <sky012877@gmail.com>
2024-07-18 17:47:44 -0700

    sd:wq

--- tests/test_2.py
+++ tests/test_2.py
@@ -2,96 +2,96 @@ import pytest
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
-# def test_1() -> None: # Pass
+def test_1() -> None: # Pass
 
-#     assert 1 == 1
-#     assert 2 == 2
-#     assert 3 == 3
-# # assert text
-# # expect(1).toBe(1)
-# # expect(1).toBe(1)
-# # expect(1).toBe(1)
-
-
-# @pytest.mark.xfail
-# def test_2() -> None: # Fail manually
-
-# # expect(1).toBe(1)
-# # expect(1).toBe(1)
-# # expect(1).toBe(1)
-#     assert 1 == 1
-#     assert 2 == 2
-#     assert 1 == 2 + 4 - 8
-# # Push to Fail
+    assert 1 == 1
+    assert 2 == 2
+    assert 3 == 3
+# assert text
+# expect(1).toBe(1)
+# expect(1).toBe(1)
+# expect(1).toBe(1)
 
 
-# def test_3() -> None: # Fail
-# # # Fail and Stop test3 and continue with next tests
-#     assert 1 == 1
-#     print("STEP continue :0")
-#     assert 1 == 2
-#  # Fail and Stop test3 and continue with next tests
-#     print("STEP continue :1")
-#     assert 1 == 1
-#     print("STEP continue :2")
-#     assert 2 == 2
+@pytest.mark.xfail
+def test_2() -> None: # Fail manually
+
+# expect(1).toBe(1)
+# expect(1).toBe(1)
+# expect(1).toBe(1)
+    assert 1 == 1
+    assert 2 == 2
+    assert 1 == 2 + 4 - 8
+# Push to Fail
 
 
-# def test_4() -> None:  # Fail
-#     errors = []
-
-#     # Fail the test and continue with next steps
-
-#     # Zero assert
-#     print("\nSTEP continue :0")
-#     step_0_passed = (1 == 2)
-#     if not step_0_passed:
-#         print("TEST FAIL Assertion 0 Error")
-#         errors.append("Assertion 0 failed")
-
-#     # First assert
-#     print("STEP continue :1")
-#     step_1_passed = (1 == 2)
-#     if not step_1_passed:
-#         print("TEST FAIL Assertion 1 Error")
-#         errors.append("Assertion 1 failed")
+def test_3() -> None: # Fail
+# # Fail and Stop test3 and continue with next tests
+    assert 1 == 1
+    print("STEP continue :0")
+    assert 1 == 2
+ # Fail and Stop test3 and continue with next tests
+    print("STEP continue :1")
+    assert 1 == 1
+    print("STEP continue :2")
+    assert 2 == 2
 
 
-#     # Second assert
-#     print("STEP continue :2")
-#     step_2_passed = (2 == 5)
-#     if not step_2_passed:
-#         print("TEST FAIL Assertion 2 Error")
-#         errors.append("Assertion 2 failed")
+def test_4() -> None:  # Fail
+    errors = []
+
+    # Fail the test and continue with next steps
+
+    # Zero assert
+    print("\nSTEP continue :0")
+    step_0_passed = (1 == 2)
+    if not step_0_passed:
+        print("TEST FAIL Assertion 0 Error")
+        errors.append("Assertion 0 failed")
+
+    # First assert
+    print("STEP continue :1")
+    step_1_passed = (1 == 2)
+    if not step_1_passed:
+        print("TEST FAIL Assertion 1 Error")
+        errors.append("Assertion 1 failed")
 
 
-#     # Third assert
-#     print("STEP continue :3")
-#     step_3_passed = (3 == 3)
-#     if not step_3_passed:
-#         print("TEST FAIL Assertion 3 Error")
-#         errors.append("Assertion 3 failed")
-
-#     # If there were any errors, fail the test at the end
-#     if errors:
-#         pytest.fail("; ".join(errors))
+    # Second assert
+    print("STEP continue :2")
+    step_2_passed = (2 == 5)
+    if not step_2_passed:
+        print("TEST FAIL Assertion 2 Error")
+        errors.append("Assertion 2 failed")
 
 
-# #
-# def test_5() -> None: # Pass
+    # Third assert
+    print("STEP continue :3")
+    step_3_passed = (3 == 3)
+    if not step_3_passed:
+        print("TEST FAIL Assertion 3 Error")
+        errors.append("Assertion 3 failed")
 
-#     assert 1 == 1
-#     assert 2 == 2
-#     try:
-#         assert 1 == 2
-# # much code
-#     except AssertionError as e:
-# # much code
-#         print(" Warning Assertion 2 Error: ", e)
+    # If there were any errors, fail the test at the end
+    if errors:
+        pytest.fail("; ".join(errors))
 
-# # # Pass the step and continue with next steps. This is when we just pass a warning message and continue
 
-# assert 1 == 1
+#
+def test_5() -> None: # Pass
+
+    assert 1 == 1
+    assert 2 == 2
+    try:
+        assert 1 == 2
+# much code
+    except AssertionError as e:
+# much code
+        print(" Warning Assertion 2 Error: ", e)
+
+# # Pass the step and continue with next steps. This is when we just pass a warning message and continue
+
+assert 1 == 1
 
 
 def test_6() -> None: # Fail
